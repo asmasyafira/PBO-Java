8772b5b08dda48dc77f5c8480e082e57e15aa574
Asma Syafira <47140064+asmasyafira@users.noreply.github.com>
2022-10-18 22:37:37 +0700

    LKPD-1

---
+++ MyProject2/src/myproject2/MyProjects02.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myproject2;
+
+/**
+ *
+ * @author user
+ */
+public class MyProjects02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("       A              SSSSSS      MMMMM          MMMMM");
+        System.out.println("     AA AA         SSS     SSS    MMM  MM      MM  MMM"); 
+        System.out.println("    AAA AAA        SSS            MMM   MMM  MMM   MMM");
+        System.out.println("   AAA   AAA        SSSS          MMM     MMMM     MMM");
+        System.out.println("  AAAAAAAAAAA         SSSSSSS     MMM      MM      MMM");
+        System.out.println(" AAA       AAA              SSS   MMM              MMM");
+        System.out.println("AAA         AAA    SSS      SSS   MMM              MMM");
+        System.out.println("AAA          AAA      SSSSSS      MMM              MMM");
+    }
+    
+}
